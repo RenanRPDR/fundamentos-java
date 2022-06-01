74219a2c9d6058d4591f2d7c6c5d551af2609228
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2022-06-01 17:01:18 -0300

    Desafio For concluido.

---
+++ src/controle/For2.java
@@ -0,0 +1,12 @@
+package controle;
+
+import com.sun.tools.jconsole.JConsoleContext;
+
+public class For2 {
+    public static void main(String[] args) {
+
+        for (int contador = 10; contador >= 0; contador -= 2) {
+            System.out.printf("Contador: %d\n", contador);
+        }
+    }
+}
