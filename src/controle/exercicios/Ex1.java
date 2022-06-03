3b3ace94bf65d0b1f290cb1713eb1ee6a207b274
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2022-06-03 15:07:41 -0300

    Exercicios - Estruturas de Controle.

---
+++ src/controle/exercicios/Ex1.java
@@ -0,0 +1,17 @@
+package controle.exercicios;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // 1. Criar um programa que receba um número e verifique se ele está entre 0 e 10 e é par;
+
+        Scanner entrada = new Scanner(System.in);
+        int numero = entrada.nextInt();
+
+        if (numero >= 0 && numero <=10) {
+
+        }
+        System.out.println("O número esta entre 0 e 10 e é par.");
+    }
+}
