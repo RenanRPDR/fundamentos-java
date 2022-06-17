2609417ebf763bfae1e7c283d90451209126270e
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2022-06-17 03:28:05 -0300

    Metodos 02.

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -18,8 +18,8 @@ public class ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double valorFinal1 = p1.preco - (1 - p1.desconto);
-        double valorFinal2 = p2.preco - (1 - p2.desconto);
+        double valorFinal1 = p1.precoComDesconto();
+        double valorFinal2 = p2.precoComDesconto(0.3);
 
         //Minha implementacao
         System.out.printf( "O valor medio do carrinho é: R$" + (p1.preco + p2.preco) / 2 + ".");
