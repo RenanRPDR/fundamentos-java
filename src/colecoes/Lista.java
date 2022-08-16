870200925b2204ad75b6be76df7c66ff64c26be9
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2022-08-15 22:36:58 -0300

    131  Lista.

--- src/colecoes/Lista.java
+++ src/colecoes/Lista.java
@@ -1,10 +1,11 @@
 package colecoes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Lista {
     public static void main(String[] args) {
-        ArrayList<Usuario> lista = new ArrayList<>();
+        List<Usuario> lista = new ArrayList<>();
 
         Usuario u1 = new Usuario("Ana");
         lista.add(u1);
