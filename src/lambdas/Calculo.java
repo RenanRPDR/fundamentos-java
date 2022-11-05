c8dfc8b1bc62d15788ead7ce967904233399f4c0
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2022-11-04 23:58:53 -0300

    174. Interfaces funcionais #2

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -4,4 +4,11 @@ package lambdas;
 public interface Calculo {
     double executar(double a, double b);
 
+    default String legal(){
+        return "Metodo default: legal, acesso direto somente pela instancia.";
+    }
+
+    static String muitoLegal(){
+        return "Metodo static: muito legal, acesso direto somente pela classe.";
+    }
 }
