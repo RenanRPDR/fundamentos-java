9846c38ae0014ead0dfc66ac8e6d8052d7a474fe
Renan Portugal do Rio <55991676+RenanRPDR@users.noreply.github.com>
2023-07-07 19:42:15 +0100

    183. Function

--- src/lambdas/Funcao.java
+++ src/lambdas/Funcao.java
@@ -8,17 +8,29 @@ public class Funcao {
         // Utilizando da interface Function
 
         Function<Integer, String> parOuImpar =
-                numero -> numero % 2 == 0 ? "Par" : "Impar";
-
-        System.out.println(parOuImpar.apply(32));
+                numero -> numero % 2 == 0 ? "Par" : "Ímpar";
 
         Function<String, String> oResultadoE =
                 valor -> "O resultado é: " + valor;
 
-        Function<String, String> empolgado = valor -> valor + "!!!";
+        Function<String, String> empolgado =
+                valor -> valor + "!!!";
 
-        String resultadoFinal = parOuImpar.andThen(oResultadoE).andThen(empolgado).apply(32);
+        Function<String, String> duvida =
+                valor -> valor + "???";
 
-        System.out.println(resultadoFinal);
+        String resultadoFinal1 = parOuImpar
+                .andThen(oResultadoE)
+                .andThen(empolgado)
+                .apply(32);
+        System.out.println(resultadoFinal1);
+
+        String resultadoFinal2 = parOuImpar
+                .andThen(oResultadoE)
+                .andThen(duvida)
+                .apply(33);
+        System.out.println(resultadoFinal2);
+
+        System.out.println(parOuImpar.apply(32));
     }
 }
