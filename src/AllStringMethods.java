a0017117a4ba1d1af891ce8bf058272d0beab31d
Renan Portugal do Rio <55991676+RenanRPDR@users.noreply.github.com>
2024-08-04 09:32:06 +0100

    194 195 196 Streams

--- src/AllStringMethods.java
+++ src/AllStringMethods.java
@@ -1,22 +1,22 @@
 public class AllStringMethods {
     public static void main(String[] args) {
-//        // charAt(int index): Retorna o caractere no índice especificado.
-//        String strAlterada = "Hello";
-//        char result = strAlterada.charAt(1); // Retorna 'e'
-//
-//        // codePointAt(int index): Retorna o ponto de código Unicode no índice especificado.
-//        String strAlterada = "Hello";
-//        int result = strAlterada.codePointAt(1); // Retorna 101, que é o ponto de código Unicode para 'e'
-//
-//        // codePointBefore(int index): Retorna o ponto de código Unicode antes do índice especificado.
-//        String strAlterada = "Hello";
-//        int result = strAlterada.codePointBefore(1); // Retorna 72, que é o ponto de código Unicode para 'H'
-//
-//        // codePointCount(int beginIndex, int endIndex):
-//        // Retorna o número de pontos de código Unicode na substrAlteradaing entre os índices especificados.
-//        String strAlterada = "Hello";
-//        int result = strAlterada.codePointCount(0, 4); // Retorna 4, pois há quatro pontos de código Unicode na substrAlteradaing "Hell"
-//
-//        // FICA PARA DEPOIS PASSAR A LIMPO
+        // charAt(int index): Retorna o caractere no índice especificado.
+        String strAlterada = "Hello";
+        char result = strAlterada.charAt(1); // Retorna 'e'
+
+        // codePointAt(int index): Retorna o ponto de código Unicode no índice especificado.
+        String strAlterada1 = "Hello";
+        int result1 = strAlterada.codePointAt(1); // Retorna 101, que é o ponto de código Unicode para 'e'
+
+        // codePointBefore(int index): Retorna o ponto de código Unicode antes do índice especificado.
+        String strAlterada2 = "Hello";
+        int result2 = strAlterada.codePointBefore(1); // Retorna 72, que é o ponto de código Unicode para 'H'
+
+        // codePointCount(int beginIndex, int endIndex):
+        // Retorna o número de pontos de código Unicode na substrAlteradaing entre os índices especificados.
+        String strAlterada3 = "Hello";
+        int result3 = strAlterada.codePointCount(0, 4); // Retorna 4, pois há quatro pontos de código Unicode na substrAlteradaing "Hell"
+
+        // FICA PARA DEPOIS PASSAR A LIMPO
     }
 }
