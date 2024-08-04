c0f675f359cd0a8c43de2accea1d7f8b526ba7b3
Renan Portugal do Rio <55991676+RenanRPDR@users.noreply.github.com>
2024-08-04 10:08:04 +0100

    197. Criando Stream

--- src/Streams/CriandoStreams.java
+++ src/Streams/CriandoStreams.java
@@ -1,5 +1,33 @@
 package Streams;
 
-public class CriandoStreams {
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
+public class CriandoStreams {
+    public static void main(String[] args) {
+
+        Consumer<String> print = System.out::print;
+        Consumer<Integer> println = System.out::println;
+
+        Stream<String> langs = Stream.of("Java ", "Lua ", "JS\n");
+
+        langs.forEach(print);
+
+        // Stream com array
+        String[] maisLangs = {"Python ", "Lisp ", "Perl ", "Go\n"};
+
+        Stream.of(maisLangs).forEach(print);
+        Arrays.stream(maisLangs).forEach(print);
+        Arrays.stream(maisLangs, 1, 4).forEach(print);
+
+        List<String> outrasLangs = Arrays.asList("C ", "PHP ", "Kotlin\n");
+        outrasLangs.stream().forEach(print);
+        outrasLangs.parallelStream().forEach(print);
+
+        // Repeticao infinta
+//        Stream.generate(() -> "a").forEach(print);
+//        Stream.iterate(0, n -> n + 1).forEach(println);
+    }
 }
